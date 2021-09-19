d64be30eeb85e3a72544ef4fdac643d3714ceb2b
Murilo Oliveira de Araujo <muritavo@outlook.com>
2021-09-19 05:23:35 -0300

    Some fixes to allow for working with multiple mfes at the same time

--- src/react/scripts/scripts/init.ts
+++ src/react/scripts/scripts/init.ts
@@ -1,6 +1,21 @@
-declare var System: any;
+require("systemjs/dist/system.min.js");
 
-System.import("/index.js").then((m: any) => {
-    m.init({})
-    return m.get("entry")
-}).then((m: any) => m());
+declare var System: any;
+declare var __react_refresh_library__: any;
+
+var $RefreshInjected$ = "__reactRefreshInjected";
+// Namespace the injected flag (if necessary) for monorepo compatibility
+if (
+  typeof __react_refresh_library__ !== "undefined" &&
+  __react_refresh_library__
+) {
+  $RefreshInjected$ += "_" + __react_refresh_library__;
+}
+delete (window as any)[$RefreshInjected$];
+
+System.import("/index.js")
+  .then((m: any) => {
+    m.init({});
+    return m.get("entry");
+  })
+  .then((m: any) => m());
--- src/react/scripts/start.ts
+++ src/react/scripts/start.ts
@@ -1,19 +1,24 @@
 #! /usr/bin/env node
 
-import DevServer from 'webpack-dev-server';
-import { join, resolve } from 'path';
-import { getArgument } from '../../shared/arguments';
+import DevServer from "webpack-dev-server";
+import { join, resolve } from "path";
+import { getArgument } from "../../shared/arguments";
 //This plugin is used to inject the bundles into the main html (when it exists)
-import { createWebpackConfiguration} from './_webpackConfiguration';
+import { createWebpackConfiguration } from "./_webpackConfiguration";
 
-const root = resolve(getArgument("root", process.env.INIT_CWD!));
+const root = resolve(getArgument("root", process.cwd()));
 const mode = "development";
 const devServer = new DevServer(
-    createWebpackConfiguration(root, mode),
-    {
-        contentBase: join(root, "public"),
-        hot: true,
-        hotOnly: true,
+  {
+    hot: true,
+    client: {
+        overlay: false,
+        progress: false
+    },
+    headers: {
+        "Access-Control-Allow-Origin": "*"
     }
+  },
+  createWebpackConfiguration(root, mode)
 );
-devServer.listen(Number(process.env.PORT));
+devServer.listen(Number(process.env.PORT || "3500"));
