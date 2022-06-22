d9cb10b510dfaa5fb52f29989082c30004bc689c
Murilo Oliveira de Araujo <muritavo@outlook.com>
2022-06-21 23:59:03 -0300

    Added declaration files

--- src/react/scripts/scripts/init.ts
+++ src/react/scripts/scripts/init.ts
@@ -2,6 +2,7 @@ require("systemjs/dist/system.min.js");
 
 declare var System: any;
 declare var __react_refresh_library__: any;
+declare var __webpack_share_scopes__: any;
 
 var $RefreshInjected$ = "__reactRefreshInjected";
 // Namespace the injected flag (if necessary) for monorepo compatibility
@@ -15,7 +16,9 @@ delete (window as any)[$RefreshInjected$];
 
 System.import("/index.js")
   .then((m: any) => {
-    m.init({});
+    try {
+      m.init(__webpack_share_scopes__.default);
+    } catch (e) {}
     return m.get("entry");
   })
   .then((m: any) => m());
