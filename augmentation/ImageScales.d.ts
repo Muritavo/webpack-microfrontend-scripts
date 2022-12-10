10d8841443aeb7029ac5058c3f0ceddb6d5dd6eb
Murilo Oliveira de Araujo <murilo.araujo@onepercent.io>
2022-12-10 12:49:09 -0300

    Refactored typings and publish

--- augmentation/ImageScales.d.ts
+++ augmentation/ImageScales.d.ts
@@ -1,13 +1,5 @@
-declare enum ImageScales {
-  SMALL = "0.5x",
-  NORMAL = "1x",
-  BIG = "2x",
-  LARGE = "3x",
-  EXTRA_LARGE = "4x",
-}
-
 declare module "*.png" {
   export const Scaled: {
-    [s in ImageScales]: string;
+    [s in import("../types/ImageScales").ImageScales]: string;
   };
 }
